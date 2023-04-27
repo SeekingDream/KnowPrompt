b13d390182d3cb69aec3059dfe30ee44e4ab1f00
SeekingDream <920730325@qq.com>
2023-04-27 01:09:21 -0500

    update local DNN

--- prompt_template.py
+++ prompt_template.py
@@ -4,21 +4,21 @@ class CodeTranslationTemplate:
     @staticmethod
     def get_instruction_1(x, src_lang, tgt_lang):
         instruction = f"Translate following {src_lang} code to {tgt_lang} code. " \
-                      f"The {src_lang} code is: \n{x}." \
+                      f"The {src_lang} code is: \n{x}" \
                       f"Only return the {tgt_lang} code."
         return instruction
 
     @staticmethod
     def get_instruction_2(x, src_lang, tgt_lang):
         instruction = f'You are given the task of translating {src_lang} code to {tgt_lang} code.' \
-                      f'The input {src_lang} code is: \n{x}. ' \
+                      f'The input {src_lang} code is: \n{x}' \
                       f'Only return the {tgt_lang} code.'
         return instruction
 
     @staticmethod
     def get_instruction_3(x, src_lang, tgt_lang):
         instruction = f"Transfer following {src_lang} code to {tgt_lang} code. " \
-                      f"The {src_lang} code is: \n {x}." \
+                      f"The {src_lang} code is: \n {x}" \
                       f"Only return the {tgt_lang} code."
         return instruction
 
@@ -27,19 +27,19 @@ class CodeSummarizationTemplate:
     @staticmethod
     def get_instruction_1(x, src_lang, tgt_lang):
         instruction = f'Summarize the following {src_lang} code with natural language description. ' \
-                      f'The {src_lang} code is: \n {x}.'
+                      f'The {src_lang} code is: \n{x}'
         return instruction
 
     @staticmethod
     def get_instruction_2(x, src_lang, tgt_lang):
         instruction = f'You are given the task of generating comments for {src_lang} code. ' \
-                      f'The {src_lang} code is: \n {x}.'
+                      f'The {src_lang} code is: \n{x}'
         return instruction
 
     @staticmethod
     def get_instruction_3(x, src_lang, tgt_lang):
         instruction = f'Generate comments the following {src_lang} code. ' \
-                      f'The {src_lang} code is: \n {x}.'
+                      f'The {src_lang} code is: \n{x}'
         return instruction
 
 
